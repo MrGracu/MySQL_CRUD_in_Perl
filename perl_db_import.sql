d549e136f7511d1949b24aaa9c73662a64571269
MrGracu <s23991@st.pwsz.nysa.pl>
2020-05-09 14:32:50 +0200

    Update perl_db_import.sql

--- perl_db_import.sql
+++ perl_db_import.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS db_perl;
 
-CREATE DATABASE db_perl DEFAULT CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE db_perl DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER 'perl_user'@'localhost' IDENTIFIED BY 'Z8nIilL3323Ee';
 GRANT ALL PRIVILEGES ON db_perl.* To 'perl_user'@'localhost';
